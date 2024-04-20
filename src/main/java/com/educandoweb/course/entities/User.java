ce144427552b5cc5863bb5269f3ec773df38d3fe
Marlonsouto <soutomarlon@gmail.com>
2024-04-20 19:58:12 -0300

    h2 database and jpa

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,8 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.io.Serial;
@@ -10,15 +13,32 @@ import java.io.Serializable;
 @Getter @Setter
 @EqualsAndHashCode
 
+@Entity
+@Table(name = "tb_users")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(name = "name")
     String name;
+
+    @Email
+    @NonNull @NotBlank
+    @Column(name = "email")
     String email;
+
+    @NonNull @NotBlank
+    @Column(name = "phone")
     String phone;
+
+    @NonNull @NotBlank
+    @Column(name = "password")
     String password;
+
+    //spring.application.name=course
 
 }
