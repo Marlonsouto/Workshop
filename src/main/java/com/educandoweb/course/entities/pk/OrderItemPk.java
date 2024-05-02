16f2e151e5fa62f53ed6eb058f4bd768a3d6cc31
Marlonsouto <soutomarlon@gmail.com>
2024-05-02 17:13:11 -0300

    created OrderItem and OrderItempk in package pk

---
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.entities.pk;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Getter @Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+}
